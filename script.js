631bf4db701573c0219e68163ab1ee393e74068e
al-shahzeb <99374818+al-shahzeb@users.noreply.github.com>
2023-07-02 19:05:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+const ele = document.getElementById('browser-info');
+
+ele.innerHTML ="You are using Chrome version Version 114.0.5735.199 (Official Build) (64-bit)";
